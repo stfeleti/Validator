b055c20d456eb0c447bd0c20ba5afc16f301f1e2
stfeleti <stfeleti@gmail.com>
2021-09-06 11:35:34 +0200

    Add project files.

---
+++ Validator/Shared/Validator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validator.Shared
+{
+    public class Validator
+    {
+        public string CardNumber { get; set; }
+        public string  Result { get; set; }
+    }
+}
